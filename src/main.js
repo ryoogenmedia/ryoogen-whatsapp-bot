65fb8ea22e14e6f4e8d68036257c23d792f83428
Bintang. <90763575+M-BintangR@users.noreply.github.com>
2024-06-08 02:32:46 +0800

    node js with express

--- src/main.js
+++ src/main.js
@@ -1,27 +1,65 @@
-// Supports ES6
-// import { create, Whatsapp } from 'venom-bot';
+const express = require("express");
 const venom = require("venom-bot");
 
+const app = express();
+const port = 3000;
+
+// Middleware untuk parsing JSON dan URL-encoded
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let client;
+
+// Buat sesi Venom-Bot
 venom
   .create({
-    session: "ryoogen-session-whatsapp", //name of session
+    session: "ryoogen-session-whatsapp", // nama sesi
   })
-  .then((client) => start(client))
+  .then((venomClient) => {
+    client = venomClient;
+    startServer();
+  })
   .catch((erro) => {
-    console.log(erro);
+    console.log("Error starting Venom:", erro);
   });
 
-function start(client) {
-  client.onMessage((message) => {
-    if (message.body === "Hi" && message.isGroupMsg === false) {
-      client
-        .sendText(message.from, "Welcome Venom 🕷")
-        .then((result) => {
-          console.log("Result: ", result); //return object success
-        })
-        .catch((erro) => {
-          console.error("Error when sending: ", erro); //return object error
-        });
-    }
+app.get("/api/getuser", async (req, res, next) => {
+  return res.status(200).json({ kocak: "sdfsa" });
+});
+
+app.post("/api/send-text", async (req, res, next) => {
+  if (!client) {
+    return res.status(500).json({ error: "Venom client not initialized" });
+  }
+
+  const { from, text } = req.body;
+
+  try {
+    const result = await client.sendText(from, text); // Menggunakan req.body.form
+    res.status(200).json({ data: result });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+app.post("/api/send-image", async (req, res, next) => {
+  if (!client) {
+    return res.status(500).json({ error: "Venom client not initialized" });
+  }
+
+  const { from, image, image_name, caption } = req.body;
+
+  try {
+    const result = await client.sendImage(from, image, image_name, caption);
+    res.status(200).json({ data: result });
+  } catch (e) {
+    console.error("Error sending image:", e);
+    res.status(500).json({ error: e.message });
+  }
+});
+
+function startServer() {
+  app.listen(port, () => {
+    console.log(`Express server listening on port ${port}`);
   });
 }
